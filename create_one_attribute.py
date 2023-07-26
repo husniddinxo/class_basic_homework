2bdbae317641f336e3b5cad5b8c59a386bbfa788
husniddinxo <husniddinbaxodirov66@gmail.com>
2023-07-26 17:57:19 +0500

    name

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,11 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+
+class Person:
+    def __init__(self,name,age):
+        self.name= name 
+        self.age = age 
+     
+
+   
+    
